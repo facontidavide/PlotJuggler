8421bacb401cfd6b1a4c96b855dc7eda0f1bfb56
Davide Faconti <davide.faconti@gmail.com>
2025-05-24 15:17:46 +0200

    try fix deps

---
+++ cmake/find_or_download_data_tamer.cmake
@@ -0,0 +1,23 @@
+function(find_or_download_data_tamer)
+
+    find_package(data_tamer QUIET)
+
+    if(data_tamer_FOUND)
+        message(STATUS "Found data_tamer in system")
+
+    elseif (NOT TARGET data_tamer_parser)
+        message(STATUS "data_tamer not found, downloading")
+        CPMAddPackage(
+            NAME data_tamer
+            GITHUB_REPOSITORY PickNikRobotics/data_tamer
+            GIT_TAG 1.0.3
+            DOWNLOAD_ONLY YES
+        )
+
+        add_library(data_tamer_parser INTERFACE )
+        target_include_directories(data_tamer_parser INTERFACE "${data_tamer_SOURCE_DIR}/data_tamer_cpp/include")
+        add_library(data_tamer::parser ALIAS data_tamer_parser)
+
+    endif()
+
+endfunction()
