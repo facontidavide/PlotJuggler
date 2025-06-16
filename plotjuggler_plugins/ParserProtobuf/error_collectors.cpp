9bea259c2216dbc79612cb0885e343e78a33eab7
Zach Davis <zdavkeos@gmail.com>
2025-06-16 16:52:54 -0500

    Fix protobuf parser
    The protobuf API changed in v30.0

--- plotjuggler_plugins/ParserProtobuf/error_collectors.cpp
+++ plotjuggler_plugins/ParserProtobuf/error_collectors.cpp
@@ -2,24 +2,24 @@
 #include <QMessageBox>
 #include <QDebug>
 
-void FileErrorCollector::AddError(const std::string& filename, int line, int,
-                                  const std::string& message)
+void FileErrorCollector::RecordError(absl::string_view filename, int line, int,
+                                     absl::string_view message)
 {
   auto msg = QString("File: [%1] Line: [%2] Message: %3\n\n")
-                 .arg(QString::fromStdString(filename))
+                 .arg(QString::fromStdString(std::string(filename)))
                  .arg(line)
-                 .arg(QString::fromStdString(message));
+                 .arg(QString::fromStdString(std::string(message)));
 
   _errors.push_back(msg);
 }
 
-void FileErrorCollector::AddWarning(const std::string& filename, int line, int,
-                                    const std::string& message)
+void FileErrorCollector::RecordWarning(absl::string_view filename, int line, int,
+                                       absl::string_view message)
 {
   auto msg = QString("Warning [%1] line %2: %3")
-                 .arg(QString::fromStdString(filename))
+                 .arg(QString::fromStdString(std::string(filename)))
                  .arg(line)
-                 .arg(QString::fromStdString(message));
+                 .arg(QString::fromStdString(std::string(message)));
   qDebug() << msg;
 }
 
