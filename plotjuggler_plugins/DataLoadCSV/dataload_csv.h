3bfa973714911af31bc8fe03ad335b30ae5b00f2
aGendy <adrianogendy29@yahoo.it>
2026-01-16 16:19:06 +0100

    Removed unused functions and added Doxygen comments to functions

--- plotjuggler_plugins/DataLoadCSV/dataload_csv.h
+++ plotjuggler_plugins/DataLoadCSV/dataload_csv.h
@@ -71,24 +71,54 @@ public:
   static constexpr const char* INDEX_AS_TIME = "__TIME_INDEX_GENERATED__";
 
 private:
-  static void ShowWarningMessageBox(const QString& title, const QString& message,
-                                    QMessageBox::StandardButton& result);
-
-  static void ShowWarningMessageBoxSkippedLines(const QString& title, const QString& message,
-                                                const QString& detailedText);
-
-  static void ShowWarningMessageBoxNonMonotonicTime(const QString& title, const QString& message,
-                                                    const QString& detailedText,
-                                                    bool& sortButtonClicked);
+  /**
+   * @brief Sets up the UI component of the class.
+   */
   void SetupUI();
 
+  /**
+   * @brief Parses the header of the input file. If the first line does not contain the header, but
+   * a set of numbers, a name will be applied automatically.
+   * @param[in] file File whose header will be read.
+   * @param[out] column_names Column names parsed from the file.
+   *
+   * @note This is a pure backend function. No QT's widgets/messageboxes (or whatever component that
+   * requires a user intervention) should be allowed. This complete separation allows to have a
+   * testable function.
+   */
   void parseHeaderLogic(QFile& file, std::vector<std::string>& column_names);
+
+  /**
+   * @brief Updates the UI counterpart of the parse header step. If the logic step (@see
+   * parseHeaderLogic) detects multiple columns with the same name, a warning message box is
+   * triggered to alert the user.
+   * @param[in] column_names Columns names to be used to update the UI.
+   */
   void parseHeaderUI(const std::vector<std::string>& column_names);
 
-  // The UI counterpart is readDataFromFile
+  /**
+   * @brief This function reads the file data and builds the corresponding plot data.
+   * @param[in] info File information.
+   * @param[out] plot_data Plot data to be shown.
+   *
+   * @note This is a pure backend function. No QT's widgets/messageboxes (or whatever component that
+   * requires a user intervention) should be allowed. This complete separation allows to have a
+   * testable function. If the function needs to interact with the user through widgets (to gain information
+   * on how to proceed), there are callbacks that are initialized by the UI counterpart. If
+   * the function is NOT called by the UI, the callbacks will keep their default behavior. This default
+   * behavior cannot include any widget/messagebox, otherwise the function won't be testable.
+   */
   bool readDataFromFileLogic(FileLoadInfo* info, PlotDataMapRef& plot_data);
 
-  // UI callbacks
+  // UI callbacks: the following UI callbacks are used to decouple the logic functions from the UI functions.
+  // This allows to have pure logic functions that are testable and that do not depend on any UI user interaction.
+  // However, sometimes the logic functions need some inputs from the user to proceed with the computation.
+  // These callbacks are set ONLY when the logic functions are called by the UI counterpart. If, instead,
+  // the logic functions are called from a Unit test (or whatever other backend unit), the callback will remain
+  // null and a default user behavior will be returned instead.
+  // Example: the launchDialogCallback is used to understand from the user which column should be used
+  // as a time index; if the function is called from the UI itself we will see the appropriate Qt widget,
+  // if instead we are calling it from a Unit Test we will have as a default return value TIME_INDEX_NOT_DEFINED.
   using LaunchDialogCallback =
       std::function<int(QFile& file, std::vector<std::string>* column_names)>;
   LaunchDialogCallback _launchDialogCallback;
