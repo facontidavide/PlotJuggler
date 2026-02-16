14d6161e3d4751df7c5f0217cf915a5a0697ef5f
avalen2022 <a.valencia.2022@alumnos.urjc.es>
2026-02-16 13:27:58 +0100

    New: Adding new CSV toolbox to improve the export csv files

---
+++ plotjuggler_plugins/ToolboxCSV/toolbox_csv.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <QtPlugin>
+#include <thread>
+#include "PlotJuggler/toolbox_base.h"
+#include "PlotJuggler/plotwidget_base.h"
+
+namespace Ui
+{
+class toolBoxUI;
+}
+
+class ToolboxCSV : public PJ::ToolboxPlugin
+{
+  Q_OBJECT
+  Q_PLUGIN_METADATA(IID "facontidavide.PlotJuggler3.Toolbox")
+  Q_INTERFACES(PJ::ToolboxPlugin)
+
+public:
+  ToolboxCSV();
+
+  ~ToolboxCSV() override;
+
+  const char* name() const override
+  {
+    return "CSV data saver";
+  }
+
+  void init(PJ::PlotDataMapRef& src_data, PJ::TransformsMap& transform_map) override;
+
+  std::pair<QWidget*, WidgetType> providedWidget() const override;
+
+public slots:
+
+  bool onShowWidget() override;
+
+private slots:
+
+  void onClosed();
+
+private:
+  QWidget* _widget;
+  Ui::toolBoxUI* ui;
+
+  PJ::PlotWidgetBase* _plot_widget = nullptr;
+  PJ::PlotDataMapRef* _plot_data = nullptr;
+  PJ::TransformsMap* _transforms = nullptr;
+
+  QStringList _dragging_curves;
+
+  bool eventFilter(QObject* obj, QEvent* ev) override;
+};
