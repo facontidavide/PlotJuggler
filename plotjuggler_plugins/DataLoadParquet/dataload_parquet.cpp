4b1b3b572d5c34e8750cbd7ddb341b6dd5cce20b
avalen2022 <a.valencia.2022@alumnos.urjc.es>
2026-02-19 13:52:18 +0100

    Fix: Fixed file_names bug

--- plotjuggler_plugins/DataLoadParquet/dataload_parquet.cpp
+++ plotjuggler_plugins/DataLoadParquet/dataload_parquet.cpp
@@ -202,6 +202,9 @@ bool DataLoadParquet::readDataFromFile(FileLoadInfo* info, PlotDataMapRef& plot_
   {
     return false;
   }
+
+  ui->listWidgetSeries->clear();
+
   infile = result.ValueOrDie();
 
   // Create Arrow FileReader
